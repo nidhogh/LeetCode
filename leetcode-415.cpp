ded512432c030306b2bd35fca4e92b594a95c749
wangjian <1536022504@qq.com>
2023-07-08 22:57:39 +0800

    优化：补0 && int add若为1则进一位

--- leetcode-415.cpp
+++ leetcode-415.cpp
@@ -1,68 +1,48 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <string>
-#include <vector>
-
 using namespace std;
 
+//add-strings
 string addStrings(string num1, string num2) {
-    bool equal = num1.length() == num2.length();
-    bool big1 = true;
-    if (num1.length() < num2.length()) {
-        big1 = false;
-    }
-    string tmp = big1 ? num1 : num2;
+    //字符串相加前先反转
     reverse(num1.begin(), num1.end());
     reverse(num2.begin(), num2.end());
-    string num3 = big1 ? num2 : num1;
-    int minLen = min(num1.length(), num2.length());
-    bool add1 = false;
-    for (int i = 0; i < minLen; i++) {
+    //保证num1到长度大于等于num2，否则交换
+    if (num1.length() < num2.length()) {
+        string tmp = num1;
+        num1 = num2;
+        num2 = tmp;
+    }
+    int len1 = num1.length();
+    int len2 = num2.length();
+    //num2如果比num1短，要补0
+    if (len1 != len2) {
+        for (int i = 0; i < len1 - len2; i++) {
+            num2 += '0';
+        }
+    }
+    //依次相加
+    int add = 0;     //是否进一位
+    for (int i = 0; i < len1; i++) {
         int n1 = num1.at(i) - '0';
         int n2 = num2.at(i) - '0';
-        int n3 = n1 + n2;
-        if (add1) {
-            n3 += 1;
-        }
-        if (n3 > 9) {
-            add1 = true;
-            n3 = n3 - 10;
-        } else {
-            add1 = false;
-        }
-        char c = '0' + n3;
-        num3[i] = c;
+        num1[i] = '0' + (n1 + n2 + add) % 10;
+        add = (n1 + n2 + add) > 9 ? 1 : 0;
     }
-    cout << "num3: " << num3 << endl;
-    reverse(num3.begin(), num3.end());
 
-    if (!equal) {
-        int subLen = max(num1.length(), num2.length()) - minLen;
-        string substr = tmp.substr(0, subLen);
-        num3 = substr + num3;
-        cout << "substr: " << substr << endl;
-        if (add1) {
-            for (int i = subLen - 1; i >= 0; i--) {
-                if (num3[i] == '9') {
-                    add1 = true;
-                    num3[i] = '0';
-                } else {
-                    add1 = false;
-                    num3[i] = num3[i] + 1;
-                    break;
-                }
-            }
-        }
+    if (add) {
+        num1 += '1';
     }
-    if (add1) {
-        num3 = '1' + num3;
-    }
-    //先拼上之前多出的部分，再在多出的部分加1，该进一位就进一位
-    cout << "num3: " << num3 << endl;
-    return num3;
+    
+    //反转num1
+    reverse(num1.begin(), num1.end());
+    
+    return num1;
 }
 
-int main() {
-    addStrings("0", "0");
-    return 0;
+int main()
+{
+    addStrings("9", "99");
+    std::cout << "hello world!\n";
 }
